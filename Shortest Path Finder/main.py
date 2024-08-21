e84dbb241271864e8e9b197ac14963a5541113e5
Reha Demircan <153535205+rdmrcn@users.noreply.github.com>
2024-08-21 15:50:58 +0300

    Add files via upload

---
+++ Shortest Path Finder/main.py
@@ -0,0 +1,88 @@
+import queue
+import time
+
+maze = [
+    ["#", "O", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", " ", "#", "#", " ", "#", "#", " ", "#"],
+    ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
+]
+
+def print_maze(maze, path=[]):
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if (i, j) in path:
+                print("X", end=" ")
+            else:
+                print(value, end=" ")
+        print()
+
+def find_start(maze, start):
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if value == start:
+                return i, j
+    return None
+
+def find_path(maze):
+    start = "O"
+    end = "X"
+    start_pos = find_start(maze, start)
+
+    q = queue.Queue()
+    q.put((start_pos, [start_pos]))
+
+    visited = set()
+
+    while not q.empty():
+        current_pos, path = q.get()
+        row, col = current_pos
+
+        print_maze(maze, path)
+        time.sleep(0.2)
+        print("\n" * 3)
+
+        if maze[row][col] == end:
+            print("Path found!")
+            return path
+
+        neighbors = find_neighbors(maze, row, col)
+        for neighbor in neighbors:
+            if neighbor in visited:
+                continue
+
+            r, c = neighbor
+            if maze[r][c] == "#":
+                continue
+
+            new_path = path + [neighbor]
+            q.put((neighbor, new_path))
+            visited.add(neighbor)
+
+    print("No path found.")
+    return None
+
+def find_neighbors(maze, row, col):
+    neighbors = []
+
+    if row > 0:  # UP
+        neighbors.append((row - 1, col))
+    if row + 1 < len(maze):  # DOWN
+        neighbors.append((row + 1, col))
+    if col > 0:  # LEFT
+        neighbors.append((row, col - 1))
+    if col + 1 < len(maze[0]):  # RIGHT
+        neighbors.append((row, col + 1))
+
+    return neighbors
+
+if __name__ == "__main__":
+    path = find_path(maze)
+    if path:
+        print("Final Path:")
+        print_maze(maze, path)
